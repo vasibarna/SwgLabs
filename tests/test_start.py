cffbd3902d941dc45456bf716b31ba7ad1385418
Vasile Barna <vasi_barna@yahoo.com>
2025-06-21 14:27:14 +0300

    update typo

--- tests/test_start.py
+++ tests/test_start.py
@@ -2,6 +2,6 @@ from playwright.async_api import Page
 
 
 def test_PageName(page: Page):
-    # test that verified the name of the page
+    # test that verifies the name of the page
     page.goto("https://www.saucedemo.com/v1/index.html")
     assert "Swag" in page.title()
