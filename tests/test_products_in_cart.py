14c595b4c6daec56cbb35f0013e52301f13779f5
Vasile Barna <vasi_barna@yahoo.com>
2025-06-24 08:58:49 +0300

    update add product

--- tests/test_products_in_cart.py
+++ tests/test_products_in_cart.py
@@ -14,7 +14,7 @@ def test_add_products_in_cart(login_logout, page: Page):
     pattern = re.compile(r"^(Sauce|Test)")
     # filter the links that starts with "Sauce" or "Test"
     product_links = [item for item in links.all_text_contents() if pattern.match(item)]
-    # iterate through all the products and add them to the shopping cart
+    # iterate through all the products, click on link and add them to the shopping cart
     for i, product in enumerate(product_links):
         # click on each link and add the product to cart
         print(f"Visiting product: {product}")
@@ -36,28 +36,26 @@ def test_add_products_in_cart(login_logout, page: Page):
 
 
 def test_remove_products_from_cart(login_logout, page: Page):
-    # get all the links from the first page
-    links = page.get_by_role("link")
-    pattern = re.compile(r"^(Sauce|Test)")
-    # filter the links that starts with "Sauce" or "Test"
-    product_links = [item for item in links.all_text_contents() if pattern.match(item)]
-    # iterate through all the products and add them to the shopping cart
-    for i, product in enumerate(product_links):
-        # click on each link and add the product to cart
-        page.get_by_role("link", name=product).click()
-        page.wait_for_load_state("networkidle")
-        page.get_by_role("button", name="ADD TO CART").click()
-        page.go_back()
+    # get all the "ADD TO CART" buttons from the first page and add each product in the cart
+    add_buttons = page.get_by_role("button", name="ADD TO CART")
+    for i in reversed(range(add_buttons.count())):
+        print(f"Adding item {i}")
+        add_buttons.nth(i).click()
         page.wait_for_load_state("networkidle")
 
-    # Shopping cart products number
+    # Go to the shopping cart and remove all items
     page.goto("https://www.saucedemo.com/v1/cart.html")
     remove_buttons = page.get_by_role("button", name="REMOVE")
     for i in reversed(range(remove_buttons.count())):
-        print(f"Removing item {i + 1}")
-        remove_buttons.nth(i).click()
-        page.wait_for_load_state("networkidle")
-    # verify the shopping cart is empty
-    shopping_cart = page.locator("#shopping_cart_container").get_by_role("link")
-    expect(shopping_cart).to_be_visible()
-    assert shopping_cart.get_attribute("name") in [None, 0, False, "0", "False", ""]
+        if i > 0:
+            # Verify the shopping cart has fewer products once clicked on "REMOVE" button
+            print(f"Removing item {i}")
+            remove_buttons.nth(i).click()
+            shopping_cart = page.locator("span.fa-layers-counter.shopping_cart_badge")
+            assert int(shopping_cart.text_content()) == i
+            page.wait_for_load_state("networkidle")
+        else:
+            # verify the shopping cart is empty
+            shopping_cart = page.locator("#shopping_cart_container").get_by_role("link")
+            expect(shopping_cart).to_be_visible()
+            assert shopping_cart.get_attribute("name") in [None, 0, False, "0", "False", ""]
